71229ebca33687ad6de00039f49d0e4fee50b714
Qijing Zheng <zqj.kaka@gmail.com>
2019-10-06 13:16:51 +0800

    new frame: pimd, cmd and rpmd

--- auto/intro_iph.el
+++ auto/intro_iph.el
@@ -4,12 +4,12 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("beamer" "8pt" "dvipsnames")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -28,6 +28,7 @@
     "frames/initialization_positions_and_momenta"
     "frames/convergence_pimd"
     "frames/frequencies_pimd"
+    "frames/pimd_cmd_rpmd"
     "frames/thanks"
     "beamer"
     "beamer10"))
