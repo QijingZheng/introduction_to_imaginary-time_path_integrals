ac52e5830b642fbf8be25a04535db728a1eb5ee8
Qijing Zheng <zqj.kaka@gmail.com>
2019-10-07 18:45:48 +0800

    add frame: pi_dyn.tex

---
+++ frames/auto/path_integral_isomorphism.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "path_integral_isomorphism"
+ (lambda ()
+   (TeX-add-symbols
+    "pointsA"
+    "rpoffset")
+   (LaTeX-add-labels
+    "eq:quantum_partition_function"
+    "eq:density_mat_and_propagator"))
+ :latex)
+
